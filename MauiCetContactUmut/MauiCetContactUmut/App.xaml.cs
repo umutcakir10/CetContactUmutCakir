df5227dcadf293d94aa936c8c2c41a19d3f7695a
umutcakir10 <ucakir81@gmail.com>
2023-12-17 12:27:58 +0300

    Cet Contact Umut

---
+++ MauiCetContactUmut/MauiCetContactUmut/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiCetContactUmut
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiCetContactUmut/MauiCetContactUmut/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiCetContactUmut
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
